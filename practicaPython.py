294ee8d935bee83098646c360d15912255ff1ac5
Ivan Villanueva <ivanv_07@hotmail.com>
2023-02-27 15:06:46 -0500

    Solucion SQL y NoSQL

---
+++ practicaPython.py
@@ -0,0 +1,14 @@
+#SQL
+""" Structure Query Language (SQL) o Lenguaje de consulta estructurado son bases de datos relacionales donde la informacion se encuentra almacenada en un conjunto de tablas, desde donde podemos acceder a dichos datos almacenados sin necesidad de modificar o manipular las tablas. Se debe tener en cuenta que primero se debe determinar la estructura de los SQL ya que si se modifica algun dato interno se puede perjudicar todo el sistema al estar compuesto por tablas relacionadas.
+
+Un ejemplo para entender este concepto es un restaurante (tabla), este restaurante posee informacion como sus empleados: meseros, cocineros, porteros. Ademas los empleados poseen elementos como edad, nacionalidad o idioma. El restaurante funcionaria como la tabla y los empleados es la informacion que esta relacionada a ella, si a futuro cambiamos un elemento de los porteros como el idioma se generaria un problema puesto que se esta cambiando un atributo no relacional con el resto.
+
+Bases de datos relacinonales: MySQL, Oracle, PostgreSQL.
+"""
+
+#NoSQL
+"""Not only SQL (NoSQL) No solo SQL son bases de datos no relacionales, tal como su nombre lo indica los datos no se almacenan en tablas sino que poseen un esquema dinamico por lo tanto cada elemento posee su propia estructura sin afectar los demas, solucionando el gran inconveniente de los SQL.
+
+Bases de datos no relacionales: MongoDB; Redis, Apache CouchDB.
+"""
+
