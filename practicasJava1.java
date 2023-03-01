ca5c18fe6d07835758744d68c29c5d92bae5d1fb
Ivan Villanueva <ivanv_07@hotmail.com>
2023-03-01 15:19:53 -0500

    desarrollo punto 2

--- practicasJava1.java
+++ practicasJava1.java
@@ -4,13 +4,13 @@ public class practicasJava1 {
     public static void main(String[] args) {
 
         //While
-        Scanner diaDeLaSemana = new Scanner(System.in);
+        /*Scanner diaDeLaSemana = new Scanner(System.in);
 
         System.out.println("Escribe un dia de la semana: ");
         String dia = diaDeLaSemana.nextLine();
         System.out.println("Has elegido " + dia);
 
-        /*switch (dia){
+        switch (dia) {
             case "Lunes" -> System.out.println(dia + " Es un dia laboral. :(");
             case "Martes" -> System.out.println(dia + " Es un dia laboral. :(");
             case "Miercoles" -> System.out.println(dia + " Es un dia laboral. :(");
@@ -18,9 +18,54 @@ public class practicasJava1 {
             case "Viernes" -> System.out.println(dia + " Es un dia laboral. :(");
             case "Sabado" -> System.out.println(dia + " No es un dia laboral. :)");
             case "Domingo" -> System.out.println(dia + " No es un dia laboral. :)");
-            default -> System.out.println("No has elegido un dia");*/
-
-
+            default -> System.out.println("No has elegido un dia");
         }
+        */
+
+        //Ventas
+        /*Scanner ventas = new Scanner(System.in);
+
+        System.out.println("Numero de manzanas vendidas: ");
+        int manzanas = ventas.nextInt();
+        System.out.println("Numero de peras vendidas: ");
+        int peras = ventas.nextInt();
+        System.out.println("Numero de aguacates vendidos: ");
+        int aguacates = ventas.nextInt();
+        System.out.println("Numero de melones vendidos: ");
+        int melones = ventas.nextInt();
+
+        System.out.println("Vendiste " + manzanas + " manzana(s).");
+        System.out.println("Vendiste " + peras + " pera(s).");
+        System.out.println("Vendiste " + aguacates + " aguacate(s).");
+        System.out.println("Vendiste " + melones + " melon(s).");
+
+        Scanner precio = new Scanner(System.in);
+
+        System.out.println("Precio de unidad manzana: ");
+        double precioManzana = precio.nextDouble();
+        System.out.println("Precio de unidad pera: ");
+        double precioPera = precio.nextDouble();
+        System.out.println("Precio de unidad aguacate: ");
+        double precioAguacate = precio.nextDouble();
+        System.out.println("Precio de unidad melon: ");
+        double precioMelon = precio.nextDouble();
+
+        System.out.println("La unidad de manzana vale: " + precioManzana);
+        System.out.println("La unidad de pera vale: " + precioPera);
+        System.out.println("La unidad de aguacate vale: " + precioAguacate);
+        System.out.println("La unidad de melon vale: " + precioMelon);
+
+        double gananciasManzana = (manzanas * precioManzana);
+        double gananciasPera = (peras * precioPera);
+        double gananciasAguacate = (aguacates * precioAguacate);
+        double gananciasMelon = (melones * precioMelon);
+
+        System.out.println("HIciste $ " + gananciasManzana + " en manzanas");
+        System.out.println("HIciste $ " + gananciasPera + " en peras");
+        System.out.println("HIciste $ " + gananciasAguacate + " en aguacates");
+        System.out.println("HIciste $ " + gananciasMelon + " en melones");
+
+        System.out.println("Las ganancias totales son de: $ " + (gananciasManzana + gananciasPera + gananciasAguacate + gananciasMelon));
+        */
     }
 }
